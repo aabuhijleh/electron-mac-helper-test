b1e41965bd7c04ed827bb11f205797812a0ef183
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2020-08-26 09:20:55 +0300

    style: format Objective-C++ code

--- mac-helper/src/mac-helper.mm
+++ mac-helper/src/mac-helper.mm
@@ -1,23 +1,24 @@
-#include <napi.h>
-#import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
+#import <Foundation/Foundation.h>
+#include <napi.h>
 
-Napi::String AreWeOnActiveSpace(const Napi::CallbackInfo& info) {
+Napi::String AreWeOnActiveSpace(const Napi::CallbackInfo &info) {
   Napi::Env env = info.Env();
 
   BOOL isOnActiveSpace = NO;
-  for (NSWindow * window in  [NSApp orderedWindows]) {
-      isOnActiveSpace = [window isOnActiveSpace];
-      if( isOnActiveSpace ){
-          break;
-      }
+  for (NSWindow *window in [NSApp orderedWindows]) {
+    isOnActiveSpace = [window isOnActiveSpace];
+    if (isOnActiveSpace) {
+      break;
+    }
   }
 
   return Napi::String::New(env, isOnActiveSpace ? "Yes" : "No");
 }
 
 Napi::Object init(Napi::Env env, Napi::Object exports) {
-  exports.Set(Napi::String::New(env, "areWeOnActiveSpace"), Napi::Function::New(env, AreWeOnActiveSpace));
+  exports.Set(Napi::String::New(env, "areWeOnActiveSpace"),
+              Napi::Function::New(env, AreWeOnActiveSpace));
   return exports;
 };
 
