50cc0a874819068275116b6df6b7f281f9beae41
Abdurrahman Abu-HIjleh <aabuhijleh@asaltech.com>
2020-08-27 20:58:09 +0300

    [WIP] feat: add a listener for Spaces change events (#1)
    * test: call callbacks from inside an Objective-C block
    
    * docs: add some comments to native code
    
    * docs: add some comments to native code

--- mac-helper/index.js
+++ mac-helper/index.js
@@ -1,3 +1,9 @@
 const macHelper = require("bindings")("mac_helper");
 
+console.log(macHelper.areWeOnActiveSpace());
+
+macHelper.listenForActiveSpaceChange((message) => {
+  console.log("Success! We got a message from native code:", message);
+});
+
 module.exports = macHelper;
--- mac-helper/src/mac-helper.mm
+++ mac-helper/src/mac-helper.mm
@@ -2,7 +2,8 @@
 #import <Foundation/Foundation.h>
 #include <napi.h>
 
-Napi::String AreWeOnActiveSpace(const Napi::CallbackInfo &info) {
+// detect if our Electron app's windows are on the active space or not
+Napi::String areWeOnActiveSpace(const Napi::CallbackInfo &info) {
   Napi::Env env = info.Env();
 
   BOOL isOnActiveSpace = NO;
@@ -16,9 +17,43 @@ Napi::String AreWeOnActiveSpace(const Napi::CallbackInfo &info) {
   return Napi::String::New(env, isOnActiveSpace ? "Yes" : "No");
 }
 
+// reference to JS callback
+Napi::FunctionReference cb;
+
+// trigger JS callback
+void triggerCallback() {
+  const Napi::Env env = cb.Env();
+  const Napi::String message = Napi::String::New(env, "space changed");
+  const std::vector<napi_value> args = {message};
+  cb.Call(args);
+}
+
+// setup the JS callback to be triggered later
+void listenForActiveSpaceChange(const Napi::CallbackInfo &info) {
+  Napi::Env env = info.Env();
+
+  cb = Napi::Persistent(info[0].As<Napi::Function>());
+  cb.SuppressDestruct();
+
+  triggerCallback(); // this works!
+
+  // subscribe to macOS spaces change event
+  [[[NSWorkspace sharedWorkspace] notificationCenter]
+      addObserverForName:NSWorkspaceActiveSpaceDidChangeNotification
+                  object:NULL
+                   queue:NULL
+              usingBlock:^(NSNotification *note) {
+                triggerCallback(); // this is never called!
+              }];
+}
+
 Napi::Object init(Napi::Env env, Napi::Object exports) {
   exports.Set(Napi::String::New(env, "areWeOnActiveSpace"),
-              Napi::Function::New(env, AreWeOnActiveSpace));
+              Napi::Function::New(env, areWeOnActiveSpace));
+
+  exports.Set(Napi::String::New(env, "listenForActiveSpaceChange"),
+              Napi::Function::New(env, listenForActiveSpaceChange));
+
   return exports;
 };
 
